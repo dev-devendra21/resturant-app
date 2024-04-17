3cff8f3b8dd2eeb3d147ff5bc0079f4c590f1365
Devendra Kumar <devendrakumaraa@gmail.com>
2024-04-17 19:51:21 +0530

    Update functionality of CartContext

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -2,16 +2,55 @@ import { createContext, useContext, useState } from 'react'
 
 const CartContext = createContext({
   cart: [],
+  addCartItem: () => { },
+  removeCartItem: () => { },
+  incrementCartItemQuantity: () => { },
+  decrementCartItemQuantity: () => { },
+  removeAllCartItems: () => { },
 })
 
 const CartContextProvider = ({ children }) => {
   const [cart, setCart] = useState([])
+
+  const addCartItem = payload => {
+    setCart(prev => [...prev, payload])
+  }
+
+  const removeCartItem = id => {
+    const remove = cart.filter(item => item.id !== id)
+    setCart(remove)
+  }
+
+  const incrementCartItemQuantity = id => {
+    const updatedCart = cart.map(eachItem =>
+      eachItem.id === id
+        ? { ...eachItem, quantity: eachItem.quantity + 1 }
+        : eachItem,
+    )
+    setCart(updatedCart)
+  }
+
+  const decrementCartItemQuantity = id => {
+    const updatedCart = cart.map(eachItem =>
+      eachItem.id === id
+        ? { ...eachItem, quantity: eachItem.quantity - 1 }
+        : eachItem,
+    )
+    setCart(updatedCart)
+  }
+
+  const removeAllCartItems = () => { }
+
   return (
     <>
       <CartContext.Provider
         value={{
           cart,
-          setCart,
+          addCartItem,
+          removeCartItem,
+          incrementCartItemQuantity,
+          decrementCartItemQuantity,
+          removeAllCartItems,
         }}
       >
         {children}
